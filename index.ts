8777a4994c0828441f7e54e5b93d8b9e84f874ef
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2026-09-21 11:47:53 +0530

    simple command

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("clawclone-build")
+  .description("openclaw clone project.")
+  .version("0.0.1");
+
+program
+  .command("wakeup")
+  .description("show the banner and pick cli or telegram mode")
+  .action(async () => {
+    console.log("Wakeup calling...");
+  });
+
+await program.parseAsync(process.argv);
